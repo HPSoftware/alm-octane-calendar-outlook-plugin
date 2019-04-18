2fdd4c4b590a5ba15509343c2b8973a41b03bb82
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-04-18 13:41:11 +0300

    Setting version 12.60.47

--- Hpe.Nga.Api.UI.Core/Properties/AssemblyInfo.cs
+++ Hpe.Nga.Api.UI.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Hpe.Nga.Api.UI.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett Packard Enterprise")]
+[assembly: AssemblyCompany("Micro Focus")]
 [assembly: AssemblyProduct("Hpe.Nga.Api.UI.Core")]
 [assembly: AssemblyCopyright("Copyright © Hewlett Packard Enterprise 2017")]
 [assembly: AssemblyTrademark("")]
@@ -44,5 +44,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("12.60.47.0")]
+[assembly: AssemblyFileVersion("12.60.47.0")]
